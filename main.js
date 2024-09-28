fde416a2b239d4ee098286ae6868da479c8a38ac
TomyBravi <tomybravi672@gmail.com>
2024-09-28 20:33:23 -0300

    segunda entrega js

--- main.js
+++ main.js
@@ -1,27 +1,59 @@
-alert("Bienvenido a Delicias de Lolo, estos son los menú disponibles para el día de hoy: \n Opción 1: Milanesa a la napolitana \n Opción 2:Canelones de verdura con salsa rosa \n Opción 3: Albóndigas con puré \n Opción 4: Porción de tarta de zapallo");
-// ¿Es correcto hacer un alert tan largo?
+const date = new Date();
 
-let menuIngresado = prompt("Ingresar N° de opción elegida");
+let day = date.getDate();
+
+let month= date.getMonth()+1; 
+
+let fecha = day + "/" + month;
+
+setTimeout(() => {
+
+    alert(`Bienvenido a Delicias de Lolo, estos son los menú disponibles para el ${fecha}`);
+
+}, 2000);
 
 
 let monto = {
+
     milanesa: "$9000",
+
     canelones: "$6750",
+
     albondigas: "$7000",
+
     tarta: "$6000",
 }
 
-const pedido = "Su pedido fue recibido. El monto total es de";
+const comidas = ["Milanesa a la napolitana", "Canelones de verdura con salsa rosa", "Albóndigas con puré", "Porción de tarta de zapallo"]
 
-if(menuIngresado == 1){
-    alert(`${pedido} ${monto.milanesa}`);
-} else if (menuIngresado ==2) {
-    alert(`${pedido} ${monto.canelones}`);
-} else if (menuIngresado ==3) {
-    alert(`${pedido} ${monto.albondigas}`);
-} else if (menuIngresado ==4) {
-    alert(`${pedido} ${monto.tarta}`);
-}
-// No se si sería mejor cambiarlo por un switch a este if else anidado
+const boton = document.getElementById("pedido");
 
-/* Debería ponerle un timeout (no se si está permitido) al alert y al prompt para que llegue a cargar el html y css primero, ya que mientras se ejecutan ambas, de fondo se ve todo blanco y no llega a cargar ni el texto. Si existe otra solución agradecería corrección/consejo sobre eso. También quería incluir un ciclo con while para que el prompt siga apareciendo hasta que se pida una de las opciones de la N° 1 a la 4 pero durante el intento se rompió*/
+boton.addEventListener("click", function(){
+
+    let menuIngresado = prompt("Ingresar N° de opción elegida");
+
+    function opciones() {
+
+        if(menuIngresado == 1){
+
+        alert(`Su pedido de ${comidas[0]} fue recibido. El monto total es de ${monto.milanesa}`);
+
+    } else if (menuIngresado ==2) {
+
+        alert(`Su pedido de ${comidas[1]} fue recibido. El monto total es de ${monto.canelones}`);
+
+    } else if (menuIngresado ==3) {
+
+        alert(`Su pedido de ${comidas[2]} fue recibido. El monto total es de ${monto.albondigas}`);
+
+    } else if (menuIngresado ==4) {
+
+        alert(`Su pedido de ${comidas[3]} fue recibido. El monto total es de ${monto.tarta}`);
+
+    } else {alert('Por favor ingrese el numero correspondiente a los menú disponibles')}
+
+    }
+
+    opciones()
+
+})

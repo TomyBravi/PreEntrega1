abda07741bad8fc0ccf26c4cf271f8b0610b94a0
TomyBravi <tomybravi672@gmail.com>
2024-10-12 23:12:15 -0300

    tercera entrega

--- main.js
+++ main.js
@@ -6,9 +6,13 @@ let month= date.getMonth()+1;
 
 let fecha = day + "/" + month;
 
+localStorage.setItem('bienvenida', 'Bienvenido a Delicias de Lolo, estos son los menú disponibles para el');
+
+let mensaje = localStorage.getItem('bienvenida');
+
 setTimeout(() => {
 
-    alert(`Bienvenido a Delicias de Lolo, estos son los menú disponibles para el ${fecha}`);
+    alert(`${mensaje} ${fecha}`);
 
 }, 2000);
 
@@ -26,6 +30,16 @@ let monto = {
 
 const comidas = ["Milanesa a la napolitana", "Canelones de verdura con salsa rosa", "Albóndigas con puré", "Porción de tarta de zapallo"]
 
+const guardarEnStorage = (clave, valor) => {
+    localStorage.setItem(clave,valor);
+}
+
+guardarEnStorage('comidas',JSON.stringify(comidas));
+
+let comidaAlmacenada = localStorage.getItem("comidas");
+
+comidaAlmacenada = JSON.parse(comidaAlmacenada);
+
 const boton = document.getElementById("pedido");
 
 boton.addEventListener("click", function(){
@@ -36,21 +50,53 @@ boton.addEventListener("click", function(){
 
         if(menuIngresado == 1){
 
-        alert(`Su pedido de ${comidas[0]} fue recibido. El monto total es de ${monto.milanesa}`);
+        alert(`Su pedido de ${comidaAlmacenada[0]} fue recibido. El monto total es de ${monto.milanesa}`);
 
     } else if (menuIngresado ==2) {
 
-        alert(`Su pedido de ${comidas[1]} fue recibido. El monto total es de ${monto.canelones}`);
+        alert(`Su pedido de ${comidaAlmacenada[1]} fue recibido. El monto total es de ${monto.canelones}`);
 
     } else if (menuIngresado ==3) {
 
-        alert(`Su pedido de ${comidas[2]} fue recibido. El monto total es de ${monto.albondigas}`);
+        alert(`Su pedido de ${comidaAlmacenada[2]} fue recibido. El monto total es de ${monto.albondigas}`);
 
     } else if (menuIngresado ==4) {
 
-        alert(`Su pedido de ${comidas[3]} fue recibido. El monto total es de ${monto.tarta}`);
+        alert(`Su pedido de ${comidaAlmacenada[3]} fue recibido. El monto total es de ${monto.tarta}`);
 
     } else {alert('Por favor ingrese el numero correspondiente a los menú disponibles')}
+
+        /*      INTENTE CAMBIAR LOS ELSE IF POR ESTE SWITCH CASE PERO LA FUNCTION EN EL BOTON EVENT LISTENER ME PIDE UNA DECLARACION
+    switch(menuIngresado){
+
+        case 1:
+
+            alert(`Su pedido de ${comidas[0]} fue recibido. El monto total es de ${monto.milanesa}`);
+            
+            break;
+
+        case 2: 
+
+            alert(`Su pedido de ${comidas[1]} fue recibido. El monto total es de ${monto.canelones}`);
+
+            break;
+
+        case 3:
+
+            alert(`Su pedido de ${comidas[2]} fue recibido. El monto total es de ${monto.albondigas}`);
+
+            break;
+
+        case 4:
+
+            alert(`Su pedido de ${comidas[3]} fue recibido. El monto total es de ${monto.tarta}`);
+            
+            break;
+
+        default:
+
+            alert('Por favor ingrese el numero correspondiente a los menú disponibles');
+    */
 
     }
 
